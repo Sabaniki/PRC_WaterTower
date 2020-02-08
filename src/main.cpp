8a0e7979f3efbc83aa5af9ef5417bad4db7f8947
Sabaniki <sawada13@mccc.jp>
2020-02-08 23:58:12 +0900

    フォーマットしました

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "Arduino.h"
 #include "UltrasonicSensor.h"
 #include "Servo.h"
+
 void setup() {
     // Serial.begin(9600);
 }
@@ -14,13 +15,13 @@ void loop() {
     auto ultrasonicSensor = UltrasonicSensor(TRIGGER_PIN, ECHO_PIN);
     auto servo = Servo();
     servo.attach(SERVO_PIN);
-    while (true) {	// 汽車が来るまで時間を無限に潰します
+    while (true) {    // 汽車が来るまで時間を無限に潰します
         double distance = 0.0;
-        for(int i = 0; i < TIMES; i++) distance += ultrasonicSensor.readDistance();
+        for (int i = 0; i < TIMES; i++) distance += ultrasonicSensor.readDistance();
         distance /= TIMES;
         if (distance < DETECTION_STANDARD_DISTANCE) break;
     }
-    delay(WAITING_TIME);	// 汽車回転待ち
+    delay(WAITING_TIME);    // 汽車回転待ち
     servo.write(SERVO_ANGLE);
-    while (true);	// 終了後に無限に捕まえる
+    while (true);    // 終了後に無限に捕まえる
 }
