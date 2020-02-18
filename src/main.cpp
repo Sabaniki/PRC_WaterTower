44d3b5036841f8bcad50b089bee8486f5e224672
Sabaniki <sawada13@mccc.jp>
2020-02-18 19:59:45 +0900

    開発終了。

--- src/main.cpp
+++ src/main.cpp
@@ -21,21 +21,23 @@ void loop() {
     servo.attach(SERVO_PIN);
     Serial.println("init was done.");
 
-    while (true) {    // 汽車が来るまで時間を無限に潰します
-        double distance = 0.0;
-        for (int i = 0; i < TIMES; i++) distance += ultrasonicSensor.readDistance();
-        distance /= TIMES;
-        Serial.print("distance: ");
-        Serial.print(distance);
-        Serial.println("cm");
-        if (DETECTION_STANDARD_DISTANCE_MIN < distance && distance < DETECTION_STANDARD_DISTANCE_MAX) break;
+    while (true) {
+        while (true) {    // 汽車が来るまで時間を無限に潰します
+            double distance = 0.0;
+            for (int i = 0; i < TIMES; i++) distance += ultrasonicSensor.readDistance();
+            distance /= TIMES;
+            Serial.print("distance: ");
+            Serial.print(distance);
+            Serial.println("cm");
+            if (DETECTION_STANDARD_DISTANCE_MIN < distance && distance < DETECTION_STANDARD_DISTANCE_MAX) break;
+        }
+        delay(WAITING_FOR_TRAIN_TIME);    // 汽車回転待ち
+        Serial.println("waiting for train done");
+        servo.write(SERVO_ANGLE);
+        delay(WAITING_FOR_WATER_SUPPLY);
+        servo.write(0);
+        Serial.println("done");
+        delay(10000);
     }
-    delay(WAITING_FOR_TRAIN_TIME);    // 汽車回転待ち
-    Serial.println("waiting for train done");
-    servo.write(SERVO_ANGLE);
-    delay(WAITING_FOR_WATER_SUPPLY);
-    servo.write(0);
-    Serial.println("done");
-    while (true);    // 終了後に無限に捕まえる
 }
 #pragma clang diagnostic pop
